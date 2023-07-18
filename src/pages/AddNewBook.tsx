0a44338fbf96232c7afef5224f81c3a08e2f1aa0
AR-Ashik-9997 <ashikur.rahman9997@gmail.com>
2023-07-18 19:49:36 +0600

    Add,update,delete is done

--- src/pages/AddNewBook.tsx
+++ src/pages/AddNewBook.tsx
@@ -1,14 +1,19 @@
-/* eslint-disable @typescript-eslint/no-floating-promises */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable no-unsafe-optional-chaining */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { usePostBooksMutation } from "@/redux/feature/books/bookApi";
 import { BookFormData } from "@/types/globalTypes";
-import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { useNavigate } from "react-router-dom";
 import Swal from "sweetalert2";
+import dummy_book from "../assets/images/dummy_books.png";
+import "react-loading-skeleton/dist/skeleton.css";
+import CardDetailsSkeliton from "@/components/Loading/CardDetailsSkeliton";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const AddNewBookPage: React.FC = () => {
   const {
@@ -19,6 +24,7 @@ const AddNewBookPage: React.FC = () => {
   } = useForm<BookFormData>();
   const [postBook] = usePostBooksMutation();
   const navigate = useNavigate();
+  const [showSkeleton, setShowSkeleton] = useState<boolean>(true);
 
   const onSubmit: SubmitHandler<BookFormData> = (data) => {
     const { title, author, genre, publicationDate } = data;
@@ -44,121 +50,141 @@ const AddNewBookPage: React.FC = () => {
     });
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSkeleton(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-semibold text-gray-900">
-          Add New Book
-        </h2>
-      </div>
-
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md p-4">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <label
-                htmlFor="title"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Title
-              </label>
-              <input
-                type="text"
-                id="title"
-                placeholder="Enter the book title"
-                className={`rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border ${
-                  errors?.title && "input-error"
-                }`}
-                {...register("title", { required: "Title is required" })}
-              />
-              {errors.title && (
-                <p className="text-red-500 text-sm mt-1">
-                  {errors.title.message}
-                </p>
-              )}
-            </div>
-
-            <div>
-              <label
-                htmlFor="author"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Author
-              </label>
-              <input
-                type="text"
-                id="author"
-                placeholder="Enter the book author"
-                className={`rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border ${
-                  errors.author && "input-error"
-                }`}
-                {...register("author", { required: "Author is required" })}
-              />
-              {errors.author && (
-                <p className="text-red-500 text-sm mt-1">
-                  {errors.author.message}
-                </p>
-              )}
-            </div>
-
-            <div>
-              <label
-                htmlFor="genre"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Genre
-              </label>
-              <input
-                type="text"
-                id="genre"
-                placeholder="Enter the book genre"
-                className={`rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border ${
-                  errors.genre && "input-error"
-                }`}
-                {...register("genre", { required: "Genre is required" })}
-              />
-              {errors.genre && (
-                <p className="text-red-500 text-sm mt-1">
-                  {errors.genre.message}
-                </p>
-              )}
-            </div>
-
-            <div>
-              <label
-                htmlFor="publicationDate"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Publication Date
-              </label>
-              <input
-                type="date"
-                id="publicationDate"
-                className={`rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border ${
-                  errors.publicationDate && "input-error"
-                }`}
-                {...register("publicationDate", {
-                  required: "Publication date is required",
-                })}
-              />
-              {errors.publicationDate && (
-                <p className="text-red-500 text-sm mt-1">
-                  {errors.publicationDate.message}
-                </p>
-              )}
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-xl shadow-sm text-xl font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+    <section className="min-h-screen py-16 flex justify-center items-center">
+      <div className="container px-4 mx-auto w-full md:max-w-[80%] 2xl:max-w-[40%]">
+        <div className="grid grid-cols-1 lg:grid-cols-2 bg-white rounded-xl shadow-lg items-center">
+          {showSkeleton ? (
+            <CardDetailsSkeliton />
+          ) : (
+            <>
+              <div className="w-full md:w-4/5 lg:w-full">
+                <div>
+                  <img
+                    className=" rounded-xl mt-4 lg:mt-0 p-4"
+                    src={dummy_book}
+                    alt="Card"
+                  />
+                </div>
+              </div>
+              <div className="p-4 md:order-2 lg:order-none">                
+                <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+                  <div>
+                    <label
+                      htmlFor="title"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Title
+                    </label>
+                    <input
+                      type="text"
+                      id="title"
+                      placeholder="Enter the book title"
+                      className={`rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border ${
+                        errors?.title && "input-error"
+                      }`}
+                      {...register("title", { required: "Title is required" })}
+                    />
+                    {errors.title && (
+                      <p className="text-red-500 text-sm mt-1">
+                        {errors.title.message}
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="author"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Author
+                    </label>
+                    <input
+                      type="text"
+                      id="author"
+                      placeholder="Enter the book author"
+                      className={`rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border ${
+                        errors.author && "input-error"
+                      }`}
+                      {...register("author", {
+                        required: "Author is required",
+                      })}
+                    />
+                    {errors.author && (
+                      <p className="text-red-500 text-sm mt-1">
+                        {errors.author.message}
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="genre"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Genre
+                    </label>
+                    <input
+                      type="text"
+                      id="genre"
+                      placeholder="Enter the book genre"
+                      className={`rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border ${
+                        errors.genre && "input-error"
+                      }`}
+                      {...register("genre", { required: "Genre is required" })}
+                    />
+                    {errors.genre && (
+                      <p className="text-red-500 text-sm mt-1">
+                        {errors.genre.message}
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <label
+                      htmlFor="publicationDate"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Publication Date
+                    </label>
+                    <input
+                      type="date"
+                      id="publicationDate"
+                      className={`rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border ${
+                        errors.publicationDate && "input-error"
+                      }`}
+                      {...register("publicationDate", {
+                        required: "Publication date is required",
+                      })}
+                    />
+                    {errors.publicationDate && (
+                      <p className="text-red-500 text-sm mt-1">
+                        {errors.publicationDate.message}
+                      </p>
+                    )}
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-xl shadow-sm text-xl font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                    >
+                      Add New Book
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </>
+          )}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/pages/UpdateBook.tsx
+++ src/pages/UpdateBook.tsx
@@ -1,20 +1,24 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-misused-promises */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable no-unsafe-optional-chaining */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import React, { useState } from "react";
-import {
-  useSingleBookQuery,
-  useUpdateBookMutation,
-} from "@/redux/feature/books/bookApi";
-import { IBook } from "@/types/globalTypes";
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { useSingleBookQuery, useUpdateBookMutation } from "@/redux/feature/books/bookApi";
+import { IBook } from "@/types/globalTypes";
 import Swal from "sweetalert2";
+import dummy_book from "../assets/images/dummy_books.png";
+import "react-loading-skeleton/dist/skeleton.css";
+import CardDetailsSkeliton from "@/components/Loading/CardDetailsSkeliton";
 
 const UpdateBookPage: React.FC = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const { data } = useSingleBookQuery(id);
+  const [showSkeleton, setShowSkeleton] = useState<boolean>(true);
   const [bookData, setBookData] = useState<Partial<IBook>>({
     title: data?.data.title,
     author: data?.data.author,
@@ -32,9 +36,11 @@ const UpdateBookPage: React.FC = () => {
       })
         .unwrap()
         .then((res) => {
-          Swal.fire("This book is successfully updated!", "You clicked the button!", "success").then(
-            () => navigate("/all-books")
-          );
+          Swal.fire(
+            "This book is successfully updated!",
+            "You clicked the button!",
+            "success"
+          ).then(() => navigate("/all-books"));
         });
     } catch (error) {
       console.error("Error updating book:", error);
@@ -47,96 +53,118 @@ const UpdateBookPage: React.FC = () => {
       [e.target.id]: e.target.value,
     });
   };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSkeleton(false);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-center sm:px-6 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-semibold text-gray-900">
-          Update Book
-        </h2>
-      </div>
+    <section className="min-h-screen py-16 flex justify-center items-center">
+      <div className="container px-4 mx-auto w-full md:max-w-[80%] 2xl:max-w-[40%]">
+        <div className="grid grid-cols-1 lg:grid-cols-2 bg-white rounded-xl shadow-lg items-center">
+          {showSkeleton ? (
+            <CardDetailsSkeliton />
+          ) : (
+            <>
+              <div className="w-full md:w-4/5 lg:w-full">
+                <div>
+                  <img
+                    className=" rounded-xl mt-4 lg:mt-0 p-4"
+                    src={dummy_book}
+                    alt="Card"
+                  />
+                </div>
+              </div>
+              <div className="p-4 md:order-2 lg:order-none">
+                <form className="space-y-6" onSubmit={handleSubmit}>
+                  <div>
+                    <label
+                      htmlFor="title"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Title
+                    </label>
+                    <input
+                      type="text"
+                      id="title"
+                      placeholder="Enter the book title"
+                      className="rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border"
+                      value={bookData.title || data?.data.title}
+                      onChange={handleInputChange}
+                    />
+                  </div>
 
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md p-4">
-        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div>
-              <label
-                htmlFor="title"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Title
-              </label>
-              <input
-                type="text"
-                id="title"
-                placeholder="Enter the book title"
-                className="rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border"
-                value={bookData.title || data?.data.title}
-                onChange={handleInputChange}
-              />
-            </div>
+                  <div>
+                    <label
+                      htmlFor="author"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Author
+                    </label>
+                    <input
+                      type="text"
+                      id="author"
+                      placeholder="Enter the book author"
+                      className="rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border"
+                      value={bookData.author || data?.data.author}
+                      onChange={handleInputChange}
+                    />
+                  </div>
 
-            <div>
-              <label
-                htmlFor="author"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Author
-              </label>
-              <input
-                type="text"
-                id="author"
-                placeholder="Enter the book author"
-                className="rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border"
-                value={bookData.author || data?.data.author}
-                onChange={handleInputChange}
-              />
-            </div>
+                  <div>
+                    <label
+                      htmlFor="genre"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Genre
+                    </label>
+                    <input
+                      type="text"
+                      id="genre"
+                      placeholder="Enter the book genre"
+                      className="rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border"
+                      value={bookData.genre || data?.data.genre}
+                      onChange={handleInputChange}
+                    />
+                  </div>
 
-            <div>
-              <label
-                htmlFor="genre"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Genre
-              </label>
-              <input
-                type="text"
-                id="genre"
-                placeholder="Enter the book genre"
-                className="rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border"
-                value={bookData.genre || data?.data.genre}
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div>
-              <label
-                htmlFor="publicationDate"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Publication Date
-              </label>
-              <input
-                type="date"
-                id="publicationDate"
-                className="rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border"
-                value={bookData.publicationDate || data?.data.publicationDate}
-                onChange={handleInputChange}
-              />
-            </div>
-            <div>
-              <button
-                type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-xl shadow-sm text-xl font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+                  <div>
+                    <label
+                      htmlFor="publicationDate"
+                      className="block text-sm font-medium text-gray-700"
+                    >
+                      Publication Date
+                    </label>
+                    <input
+                      type="date"
+                      id="publicationDate"
+                      className="rounded-xl p-2 border-t mr-0 mb-2 sm:mr-2 sm:mb-0 sm:border-b sm:border-l text-gray-800 border-yellow-700 bg-white w-full focus:outline-none focus:border-yellow-700 focus:ring-0 border"
+                      value={
+                        bookData.publicationDate || data?.data.publicationDate
+                      }
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      className="w-full flex justify-center py-2 px-4 border border-transparent rounded-xl shadow-sm text-xl font-medium text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+                    >
+                      Update Book
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </>
+          )}
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
